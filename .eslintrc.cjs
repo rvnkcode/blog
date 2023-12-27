29174745efee2dc18a9108063dd52119960aecd5
rvnkcode <sykim.p508@gmail.com>
2023-12-27 10:10:48 +0900

    Configure eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+// https://github.com/ota-meshi/eslint-plugin-astro
+module.exports = {
+  extends: [
+    "plugin:astro/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  ],
+}
